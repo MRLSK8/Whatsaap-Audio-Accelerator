3d5bedfa1263eaa04b48a5779636a3a1220c2027
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-04-27 22:36:41 -0300

    Several options of  speed added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const speeds = [{ '0.5x': 0.5 }, { '1x': 1 }, { '1.5x': 1.5 }, { '2x': 2 }];
+let currentSpeed = speeds[1];
+
+function getKeyValue(currentSpeed) {
+  return Object.keys(currentSpeed);
+}
+
 const interval = setInterval(() => {
   const header = document.querySelector('._3auIg');
 
@@ -6,15 +13,31 @@ const interval = setInterval(() => {
 
     const button = document.createElement('button');
 
-    button.innerHTML = '2x';
+    button.innerHTML = getKeyValue(currentSpeed);
     button.classList.add('twoTimesButton');
-    
+
     button.addEventListener('click', () => {
       const audios = document.querySelectorAll('audio');
-      audios.forEach(audio => {
-        audio.playbackRate = 2;
-      })
-    })
+      const indexOfCurrtentSpeed = speeds.findIndex(
+        (speed) => speed === currentSpeed
+      );
+
+      if (audios.length) {
+        currentSpeed =
+          speeds.length - 1 !== indexOfCurrtentSpeed
+            ? speeds[indexOfCurrtentSpeed + 1]
+            : speeds[0];
+
+        button.innerHTML = getKeyValue(currentSpeed);
+
+        audios.forEach((audio) => {
+          audio.onplay = () =>
+            (audio.playbackRate = currentSpeed[getKeyValue(currentSpeed)]);
+
+          audio.playbackRate = currentSpeed[getKeyValue(currentSpeed)];
+        });
+      }
+    });
 
     header.appendChild(button);
   }
